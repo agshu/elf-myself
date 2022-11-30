1093c4d4b4c506564d66d9a1693876660a56078c
Erik Meurk <60180870+erikaxelmeurk@users.noreply.github.com>
2022-11-30 11:39:20 +0100

    Added ginger bread man

--- Assets/Scripts/AttachToTree.cs
+++ Assets/Scripts/AttachToTree.cs
@@ -27,15 +27,15 @@ public class AttachToTree : MonoBehaviour
             
             //when dropped => kolla om distans < 0.1. om ja => stäng av gravitation och placera på closestPoint
             // om inte <0.1 ha kvar gravity
-            if (distance < 0.02) {
+            if (distance < 0.01) {
                 rb.transform.position = closestPoint;
-                rb.useGravity = false;
+                //rb.useGravity = false;
                 rb.isKinematic = true;
                 //rb.constraints.FreezeRotation = true;
                 //rb.constraints.FreezePosition = true;
                 //rb.constraints = RigidbodyConstraints.FreezePosition;
             } else {
-                    rb.useGravity = true;
+                    //rb.useGravity = true;
                     rb.isKinematic = false;
                     //rb.constraints = RigidbodyConstraints.None;
             }
