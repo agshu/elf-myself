1a83d0bf74cd31d0acfaf7a586d7e6522aefca9c
Erik Meurk <60180870+erikaxelmeurk@users.noreply.github.com>
2022-12-16 11:49:26 +0100

    Removed some lights

--- Assets/TakeOwnershipOnStart.cs
+++ Assets/TakeOwnershipOnStart.cs
@@ -7,20 +7,22 @@ using UnityEngine.XR.Interaction.Toolkit;
 public class TakeOwnershipOnStart : MonoBehaviour
 {
     private RealtimeTransform realtimeTransform;
+    private int connected = 0;
     void Start()
     {
         realtimeTransform = GetComponent<RealtimeTransform>();
-
     }
         
 
     // Update is called once per frame
     void Update()
     {
-        if (Application.platform == RuntimePlatform.WindowsEditor) {
+        if (connected == 0 && Application.platform == RuntimePlatform.WindowsEditor) {
             realtimeTransform.RequestOwnership();
-        } else if (Application.platform == RuntimePlatform.WindowsPlayer) {
+            connected = 1;
+        } else if (connected == 0 && Application.platform == RuntimePlatform.WindowsPlayer) {
             realtimeTransform.RequestOwnership();
+            connected = 1;
         }
     }
 }
